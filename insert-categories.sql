be20b81ee35e4a0781c8f9d0b9bb6796c03a3481
Fabriciope <fabricioalvespa@gmail.com>
2023-05-28 20:14:29 -0300

    add arquivo insert-categories.sql

---
+++ insert-categories.sql
@@ -0,0 +1,10 @@
+USE TechNews;
+
+INSERT INTO categories (category, uri)
+			VALUES     ('Programação', 'programacao'),
+					   ('Inteligência artificial', 'inteligencia-artificial'),
+                       ('Internet das coisas', 'internet-das-coisas'),
+                       ('Hardware', 'hardware'),
+					   ('Robótica', 'robotica'),
+                       ('Cibersegurança', 'ciberseguranca'),
+                       ('Aparelhos eletrônicos', 'aparelhos-eletronicos');
