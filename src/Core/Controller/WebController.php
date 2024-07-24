8598a8b235f15d736de8143f162a275d62355287
Fabriciope <fabricioalvespa@gmail.com>
2024-07-24 19:14:38 -0300

    revert: change templates directory's string level when passed to TemplatesEngine

--- src/Core/Controller/WebController.php
+++ src/Core/Controller/WebController.php
@@ -31,11 +31,11 @@ abstract class WebController implements FlashMessagesPublisher
     *
     * @throws \InvalidArgumentException
     */
-    protected function setUpTemplatesEngine(string $viewsPath = __DIR__.'/../../../../resources/templates/views/'): void
+    protected function setUpTemplatesEngine(string $viewsPath = __DIR__.'/../../../resources/templates/views/'): void
     {
         $this->templatesEngine = new TemplatesEngine($viewsPath);
-        $this->getTemplatesEngine()->addFolder('layouts', __DIR__.'/../../../../resources/templates/views/layouts/');
-        $this->getTemplatesEngine()->addFolder('includes', __DIR__.'/../../../../resources/templates/views/includes/');
+        $this->getTemplatesEngine()->addFolder('layouts', __DIR__.'/../../../resources/templates/views/layouts/');
+        $this->getTemplatesEngine()->addFolder('includes', __DIR__.'/../../../resources/templates/views/includes/');
     }
 
     protected function getTemplatesEngine(): TemplatesEngine
